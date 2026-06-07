001711a81367b6246a89db2b7b3764ed8b073a43
RasmusOlesk <rasmus.olesk12@gmail.com>
2026-06-07 21:37:26 +0300

    uuendus, et läheks tööle github pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/React_MS_SQL/',   
+  base: '/React_MS_SQL/',
   plugins: [react()],
 })
