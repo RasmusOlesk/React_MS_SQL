98d54f43095abe07ab3273592fc203ccc78bb014
RasmusOlesk <rasmus.olesk12@gmail.com>
2026-06-07 21:10:27 +0300

    Muudsin repo nimel mõne tähe väikseks täheks

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/REACT_MS_SQL/',   
+  base: '/React_MS_SQL/',   
   plugins: [react()],
 })
